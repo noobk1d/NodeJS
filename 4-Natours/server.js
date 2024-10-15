d76253758016ec2b179e943396bdc3c644ce56bd
noobk1d <practicewithmi1@gmail.com>
2024-10-15 12:51:44 +0530

    Mongodb Initiated

--- 4-Natours/server.js
+++ 4-Natours/server.js
@@ -1,5 +1,19 @@
 const dotenv = require('dotenv');
 const app = require('./app');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: './config.env' });
+//DB CONNECTION
+// const db = process.env.DATABASE.replace('<db_password>', process.env.PASSWORD);
+// const db = process.env.DATABASE;
+const db = 'mongodb://localhost:27017/demo';
+mongoose.connect(db).then(async (con) => {
+  console.log(con.connection.readyState);
+
+  const number = await con.connection.db.listCollections().toArray();
+  console.log(number.length);
+  console.log('Connection successful');
+});
 
 //ENVIRONMENT VARIABLE
 dotenv.config({ path: './config.env' });
